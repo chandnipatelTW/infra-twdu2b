aa7ce98806ac9c7c4c7c84dbe3593d394c577d35
Young, Darren <darrenyoung89@gmail.com>
2019-11-27 13:14:41 +0530

    Darren add sns policy to allow events to publish to failing emr step topic

--- modules/training_emr_cluster/monitoring.tf
+++ modules/training_emr_cluster/monitoring.tf
@@ -26,3 +26,22 @@ resource "aws_cloudwatch_event_target" "failing_emr_step_target" {
 resource "aws_sns_topic" "failing_emr_steps" {
   name = "failing_emr_steps"
 }
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = "${aws_sns_topic.failing_emr_steps.arn}"
+  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = ["${aws_sns_topic.failing_emr_steps.arn}"]
+  }
+}
