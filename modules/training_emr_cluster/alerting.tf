108857138f3be192aa252e2fda1b913c6f917244
Young, Darren <darrenyoung89@gmail.com>
2019-11-27 15:34:33 +0530

    Emily/Darren add emr step failure alert. Increase frequency of alert checks

--- modules/training_emr_cluster/alerting.tf
+++ modules/training_emr_cluster/alerting.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_metric_alarm" "file_does_not_exist" {
   evaluation_periods        = "2"
   metric_name               = "file-exists"
   namespace                 = "2Wheelers_DeliveryFile"
-  period                    = "300"
+  period                    = "60"
   extended_statistic        = "p99"
   threshold                 = "1"
   alarm_description         = "Monitors the existance of a delivery file"
@@ -17,9 +17,23 @@ resource "aws_cloudwatch_metric_alarm" "file_not_updated" {
   evaluation_periods        = "2"
   metric_name               = "file-updated"
   namespace                 = "2Wheelers_DeliveryFile"
-  period                    = "300"
+  period                    = "60"
   extended_statistic        = "p99"
   threshold                 = "1"
   alarm_description         = "Monitors the delivery file having been updated"
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "emr_step_failure" {
+  alarm_name                = "emr-step-failure-alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  treat_missing_data        = "notBreaching"
+  alarm_description         = "Monitors failures in EMR steps"
   insufficient_data_actions = []
 }
