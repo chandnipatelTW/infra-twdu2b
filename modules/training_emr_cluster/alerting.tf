27c32c1f7cd7c0b83178244a192272c2bb8be3a8
Young, Darren <darrenyoung89@gmail.com>
2019-11-27 14:40:33 +0530

    Darren/Emily add delivery file alarms

---
+++ modules/training_emr_cluster/alerting.tf
@@ -0,0 +1,25 @@
+resource "aws_cloudwatch_metric_alarm" "file_does_not_exist" {
+  alarm_name                = "file-does-not-exist-alarm"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "file-exists"
+  namespace                 = "2Wheelers_DeliveryFile"
+  period                    = "300"
+  statistic                 = "p99"
+  threshold                 = "1"
+  alarm_description         = "Monitors the existance of a delivery file"
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "file_not_updated" {
+  alarm_name                = "file-not-updated-alarm"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "file-updated"
+  namespace                 = "2Wheelers_DeliveryFile"
+  period                    = "300"
+  statistic                 = "p99"
+  threshold                 = "1"
+  alarm_description         = "Monitors the delivery file having been updated"
+  insufficient_data_actions = []
+}
