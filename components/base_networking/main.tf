a2209a497f5774187c17bf1ee8bce6d466a93560
Andres Salazar <ssalazar@thoughtworks.com>
2019-11-28 17:40:49 +0530

    [maira / gonzalo / asalazar] Adding a new module to create a new vpc for test env

--- components/base_networking/main.tf
+++ components/base_networking/main.tf
@@ -19,3 +19,16 @@ module "training_vpc" {
     "${var.aws_region}c"
   ]
 }
+
+module "training_vpc_dev" {
+  source = "../../modules/base_networking"
+
+  deployment_identifier = "data-eng-${var.cohort}${var.env}" //data-eng-twdu2b-dev
+  vpc_cidr              = "10.0.0.0/16"
+  private_dns_zone_name = "${var.cohort}${var.env}.training"
+  availability_zones = [
+    "${var.aws_region}a",
+    "${var.aws_region}b",
+    "${var.aws_region}c"
+  ]
+}
