5c37d49e1a6193088896fcf0d54d40f3c749da19
Andres Salazar <ssalazar@thoughtworks.com>
2019-11-28 13:58:16 +0530

    [asalazar] - Remove changes

--- modules/base_networking/dns.tf
+++ modules/base_networking/dns.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "private_zone" {
-  name = "${var.private_dns_zone_name}${var.env}"
+  name = "${var.private_dns_zone_name}"
   vpc {
     vpc_id = "${aws_vpc.vpc.id}"
   }
