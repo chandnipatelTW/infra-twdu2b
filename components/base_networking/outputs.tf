cf42e7d0bd0d282b6e12cb931fb84601b175fd32
Andres Salazar <ssalazar@thoughtworks.com>
2019-11-28 17:43:28 +0530

    [maira / gonzalo / asalazar] Fixing a new module to create a new vpc for test env

--- components/base_networking/outputs.tf
+++ components/base_networking/outputs.tf
@@ -18,11 +18,6 @@ output "dns_zone_id" {
   value       = "${module.training_vpc.dns_zone_id}"
 }
 
-output "dns_zone_dev_id" {
-  description = "ID of the private DNS zone attached to the VPC."
-  value       = "${module.training_vpc.dns_zone_dev_id}"
-}
-
 output "vpc_default_security_group_id" {
   description = "ID of the default SG for the VPC"
   value       = "${module.training_vpc.vpc_default_security_group_id}"
