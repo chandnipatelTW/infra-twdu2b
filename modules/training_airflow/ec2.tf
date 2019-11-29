1492a616374f0d0872e20982b5abcd955782ca2e
Andres Mulin <andres.mulin@thoughtworks.com>
2019-11-29 14:16:16 +0530

    [Lorena/Gonzalo/Andres] user data added as script

--- modules/training_airflow/ec2.tf
+++ modules/training_airflow/ec2.tf
@@ -9,8 +9,28 @@ resource "aws_instance" "airflow" {
 
   user_data = <<EOF
           #! /bin/bash
-          pwd
-          ls -al
+          set -x
+
+          export LC_CTYPE=en_US.UTF-8
+          export AIRFLOW_HOME=/usr/local/airflow
+
+          sudo amazon-linux-extras install -y epel
+
+          sudo yum update -y
+
+          sudo yum install -y python-pip
+          sudo yum install -y gcc gcc-c++
+          sudo pip install apache-airflow
+          sudo pip install apache-airflow[crypto]
+          sudo pip install apache-airflow[postgres,jdbc]
+
+          sudo chown -R ec2-user: ${AIRFLOW_HOME}
+          
+          airflow initdb
+
+          nohup airflow webserver -p 8080 -D
+          nohup airflow scheduler -D
+
   EOF
 
   tags = "${merge(
