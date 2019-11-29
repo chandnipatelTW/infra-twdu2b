df1ca7bee76adb610a0a8e6236ebc77a37d1b853
Andres Mulin <andres.mulin@thoughtworks.com>
2019-11-29 14:01:04 +0530

    [Lorena/Gonzalo/Andres] still looking for the path

--- modules/training_airflow/ec2.tf
+++ modules/training_airflow/ec2.tf
@@ -7,7 +7,11 @@ resource "aws_instance" "airflow" {
   key_name               = "${var.ec2_key_pair}"
   iam_instance_profile   = "${aws_iam_instance_profile.airflow.name}"
 
-	user_data = "${file("./install_airflow.sh")}"
+  user_data = << EOF
+      #! /bin/bash
+      pwd
+      ls -al
+      EOF
 
   tags = "${merge(
     local.common_tags,
