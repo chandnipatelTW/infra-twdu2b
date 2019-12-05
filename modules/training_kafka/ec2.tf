49a04fb49c41b0c74f42dce65a708088dab4e388
Pietra Thamy Sofia Da Fonseca Freitas <pietrasofia@hotmail.com>
2019-12-05 09:58:15 +0530

    [Gonzalo/Pietra] Add more kafka instances.

--- modules/training_kafka/ec2.tf
+++ modules/training_kafka/ec2.tf
@@ -13,3 +13,35 @@ resource "aws_instance" "kafka" {
     )
   )}"
 }
+
+resource "aws_instance" "kafka2" {
+  ami                    = "${data.aws_ami.training_kafka.image_id}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = ["${aws_security_group.kafka.id}"]
+  subnet_id              = "${var.subnet_id}"
+  key_name               = "${var.ec2_key_pair}"
+  iam_instance_profile   = "${aws_iam_instance_profile.kafka.name}"
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "kafka-${var.deployment_identifier}"
+    )
+  )}"
+}
+
+resource "aws_instance" "kafka3" {
+  ami                    = "${data.aws_ami.training_kafka.image_id}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = ["${aws_security_group.kafka.id}"]
+  subnet_id              = "${var.subnet_id}"
+  key_name               = "${var.ec2_key_pair}"
+  iam_instance_profile   = "${aws_iam_instance_profile.kafka.name}"
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "kafka-${var.deployment_identifier}"
+    )
+  )}"
+}
